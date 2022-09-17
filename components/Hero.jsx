cf58c5589717190290a68bc57df9fc35b2c6af81
Hao Jie <haojie9777@gmail.com>
2022-09-18 02:58:03 +0800

    aiyo

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
         <div className='p-5 text-white z-[2] mt-[-10rem]'>
           <h1 className='text-5xl font-bold'>Hao Jie</h1>
-          <p className='py-5 text-xl'>// Software engineer based in Singapore</p>
+          <p className='py-5 text-xl'>{'//'} Software engineer based in Singapore</p>
           <p className='py-3 text-xl'>Currently a backend engineer working in the financial sector,<br/>
           handling challenges relating to microservices and distributed systems.<br/>
           </p>
