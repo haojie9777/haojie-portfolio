30e13ae3d4e7bc99e4f1dc22e4fac14e7f33727e
Hao Jie <haojie9777@gmail.com>
2022-09-17 17:46:10 +0800

    configured fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+//may only import css styles in here 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+ 
