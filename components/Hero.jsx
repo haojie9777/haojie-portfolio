1d7807979f2c24459f335dfe2e8905289675e47f
Hao Jie <haojie9777@gmail.com>
2022-09-17 21:31:25 +0800

    navbar completed

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 //component generated w shortcut "rafce"
-const Hero = () => {
+const Hero = ({heading, message}) => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
       {/* Overlay"*/}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] '/>
-        <div className='p-5 text-white z-[2] ml-[-10rem] mt-[-10rem]'>
-          <h2 className='text-5xl font-bold'>Heading</h2>
-          <p className='py-5 text-xl'>Message</p>
-          <button className='px-8 py-2 border'>Click this</button>
+        <div className='p-5 text-white z-[2] mt-[-10rem]'>
+          <h2 className='text-5xl font-bold'>{heading}</h2>
+          <p className='py-5 text-xl'>{message}</p>
+          <button className='px-8 py-2 border'>Contact for Opportunities</button>
         </div>
     </div>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from "../components/Navbar"
 
 //may only import css styles in here 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Navbar/>
+  <Component {...pageProps} />
+  </>
+  );
 }
 
 export default MyApp
