4028ee486d570b8720a6bebafd2dfa8809b4adf0
Hao Jie <haojie9777@gmail.com>
2022-09-17 19:21:20 +0800

    added images and template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,7 @@ export default function Document() {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet" />
-
-
+                <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
             </Head>
             <body>
                 <Main />
