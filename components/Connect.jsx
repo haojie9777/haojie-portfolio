0a9ce3741988ef4754a8ce7cef0949da6c842315
Hao Jie <haojie9777@gmail.com>
2022-09-26 01:14:16 +0800

    fix some stuff

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -4,8 +4,12 @@ const Connect = () => {
   return (
     <div id='connect' className='flex items-center justify-center h-screen'>
     <div className='p-5 text-white z-[2] mt-[-10rem]'>
-      <h2 className='text-5xl font-bold'>Connect (work in progress...)</h2>
-      <p className='py-5 text-xl'>@haojie9777</p>
+      <h2 className='text-5xl font-bold'>Connect With Me!</h2>
+      <p className='py-5 text-l'>Email: </p>
+      <p className='py-5 text-l'>Linkedin: </p>
+      <p className='py-5 text-l'>Github: </p>
+
+
     </div>
 </div>
   )
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id='hero' className='flex items-center justify-center h-screen '>
-
-        <div className='p-5 text-white z-[2] mt-[-2rem]'>
-          <h1 className='text-7xl font-bold'>Hao Jie</h1>
-          <p className='py-5 text-xl'>Software engineer based in Singapore</p>
-          <p className='py-3 text-xl'>Currently a backend engineer working in the financial sector,<br/>
-          handling challenges pertaining to microservices and distributed systems.<br/>
+    <div id='hero' className='flex items-center justify-center h-screen'>
+        <div className='p-5 text-white mt-[-6rem]'>
+          <h1 className='text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-pink-800'>Hao Jie</h1>
+          <p className='text-l'>Software engineer / tech enthusiast / cat lover🐾</p>
+          <p className='mt-10 text-l'>I am predominantly a backend developer,
+           with a keen interest <br></br> in leveraging technology to empower the lives of people around me.<br/>
           </p>
-          <p className='py-7 text-xl'>Feel free to connect with me regarding any opportunities, or even for coffee :D</p>
+          <p className='mt-2 text-l'>Feel free to connect with me regarding any opportunities and / or for coffee :D</p>
         </div>
     </div>
   )
