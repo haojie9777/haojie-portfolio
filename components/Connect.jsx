42b61615ce16fad772df6d9ba70c735f6fa1cab7
haojie.tan <haojie.tan@maribank.com.sg>
2022-11-23 17:23:04 +0800

    f

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -5,9 +5,9 @@ const Connect = () => {
     <div id='connect' className='flex items-center justify-center h-screen'>
     <div className='p-5 text-white z-[2] mt-[-10rem]'>
       <h2 className='text-5xl font-bold'>Connect With Me!</h2>
-      <p className='py-5 text-l'>Email: </p>
-      <p className='py-5 text-l'>Linkedin: </p>
-      <p className='py-5 text-l'>Github: </p>
+      <p className='py-5 text-l'>haojie9777@gmail.com</p>
+      <p className='py-5 text-l'>Linkedin:  </p>
+      <p className='py-5 text-l'>@haojie9777</p>
 
 
     </div>
