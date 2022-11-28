d132b0a6f428f90565b15d2fed3f763611af8990
haojie.tan <haojie.tan@maribank.com.sg>
2022-11-28 12:00:35 +0800

    d

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,11 +4,11 @@ const Hero = () => {
   return (
     <div id='hero' className='flex flex-col justify-center items-center h-screen mt-[100px] p-5'>
       <div className='mb-10'>
-        <img src="/cat-gif.gif"></img>
+        <img className = 'animate-pulse' src="/cat-gif.gif"></img>
       </div>
-        <h1 className='text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-pink-800'>Hao Jie</h1>
-        <p className='text-l text-white'>Software Engineer / Tech Enthusiast / Caffeine Junkie / Cat Person🐾</p>
-        <p className='mt-5 text-l text-justify text-white'>I am predominantly a backend developer, with a keen interest in leveraging technology to empower the lives of people around me. 
+        <h1 className='mr-auto text-7xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400'>Hao Jie</h1>
+        <p className='text-amber-400 mr-auto'>Software Engineer / Tech Enthusiast / Caffeine Junkie / Cat Person🐾</p>
+        <p className='mt-10 text-justify text-white'>I am predominantly a backend developer, with a keen interest in leveraging technology to empower the lives of people around me. 
         In my current job, I am involved in designing and engineering distributed system solutions
           that are highly scalable and reliable. Feel free to connect with me regarding any opportunities or for coffee :D
         </p>
