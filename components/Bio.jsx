4a6806be52decfab7918623bdc935bfd63dc4537
Hao Jie <haojie9777@gmail.com>
2022-12-24 09:49:14 +0800

    fixed bug

--- components/Bio.jsx
+++ components/Bio.jsx
@@ -3,7 +3,7 @@ import Timeline from '../components/Timeline'
 
 const Bio = () => {
   return (
-    <div id='bio' className='flex flex-col items-center justify-center h-screen text-white mt-10'>
+    <div id='bio' className='flex flex-col items-center justify-center h-screen text-white mt-40'>
       <h2 className='text-7xl font-bold mb-5'>Bio</h2>
       <Timeline/>
 </div>
