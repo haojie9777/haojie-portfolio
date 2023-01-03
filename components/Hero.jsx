1f93cbf5929bdfd3ad82f4137aa82bc2fb9090b1
Hao Jie <haojie9777@gmail.com>
2023-01-03 22:21:21 +0800

    added resume

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,17 +6,24 @@ const Hero = () => {
     <div id='hero' className='
      flex flex-col justify-center items-center h-screen mt-[100px]'>
       <div className='mb-10'>
-        <Image width="300" height="300" src="/hiking-pic-ok.png" quality = '100'></Image>
+        <Image width="300" height="300" src="/hiking-pic-ok.png" quality='100'></Image>
       </div>
-        <h1 className='to-animate opacity-0 transition-all duration-1000 mr-auto text-5xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400'>Hao Jie</h1>
-        <p className='to-animate opacity-0 transition-all duration-1000 text-amber-400 mr-auto text-sm'>Software Engineer / Tech Enthusiast / Caffeine Junkie / Cat Person 🐾</p>
-        <p className='to-animate opacity-0 transition-all duration-1000 mt-10 text-justify text-white'>I am predominantly a backend developer, but I do dapple in frontend sometimes. 
+      <h1 className='to-animate opacity-0 transition-all duration-1000 mr-auto text-5xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400'>Hao Jie</h1>
+      <p className='to-animate opacity-0 transition-all duration-1000 text-amber-400 mr-auto text-sm'>Software Engineer / Tech Enthusiast / Caffeine Junkie / Cat Person 🐾</p>
+      <p className='to-animate opacity-0 transition-all duration-1000 mt-10 text-justify text-white'>I am predominantly a backend developer, but I do dapple in frontend sometimes.
         I wish to leverage technology to empower the lives of people around me. I am involved in designing and engineering distributed system solutions
-          that are highly scalable and reliable in my current job. Feel free to connect with me regarding anything :D
-        </p>
-  
-      
+        that are highly scalable and reliable in my current job. Feel free to connect with me regarding anything :D
+      </p>
+      <div className='to-animate opacity-0 transition-all duration-1000 mt-5 mb-10'>
+        <a href="/HaoJie_Resume_030122.pdf" target="_blank" rel="noopener noreferrer">
+          <button className="bg-transparent hover:bg-green-500 text-xl text-white hover:text-white
+         py-2 px-4 border border-emerald-500 hover:border-transparent rounded">
+            My Resume
+          </button>
+        </a>
       </div>
+
+    </div>
   )
 }
 export default Hero
