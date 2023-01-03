d9ed92a18178d8ce3710c20f4297ffd97ee37934
Hao Jie <haojie9777@gmail.com>
2023-01-03 23:11:27 +0800

    typo

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       </div>
       <h1 className='to-animate opacity-0 transition-all duration-1000 mr-auto text-5xl text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400'>Hao Jie</h1>
       <p className='to-animate opacity-0 transition-all duration-1000 text-amber-400 mr-auto text-sm'>Software Engineer / Tech Enthusiast / Caffeine Junkie / Cat Person 🐾</p>
-      <p className='to-animate opacity-0 transition-all duration-1000 mt-10 text-justify text-white'>I am predominantly a backend developer, but I do dapple in frontend sometimes.
+      <p className='to-animate opacity-0 transition-all duration-1000 mt-10 text-justify text-white'>I am predominantly a backend developer, but I do dabble in frontend sometimes.
         I wish to leverage technology to empower the lives of people around me. I am involved in designing and engineering distributed system solutions
         that are highly scalable and reliable in my current job. Feel free to connect with me regarding anything :D
       </p>
