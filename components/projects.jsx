6a66e51d68e7792631cdcf1688aeea49548c3550
Hao Jie <haojie9777@gmail.com>
2023-01-15 21:42:43 +0800

    created works component

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const projects = () => {
-  return (
-    <div id = "projects">
-        projects</div>
-  )
+    return (
+        <div id="projects" className='
+    flex flex-col items-center justify-center h-screen text-white'>
+            <h2 className='text-7xl font-bold '>Works</h2>
+            <div className='to-animate opacity-0 transition-all duration-1000 first-letter:flex flex-col items-center justify-center'>
+            Hello
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default projects
