0b29a9c828059637f63ee986a81e3642a9844242
Hao Jie <haojie9777@gmail.com>
2023-01-28 02:07:41 +0800

    fixed warnings

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,14 +9,17 @@ function Navbar() {
         <div className='fixed left-0 -top-3 w-full z-10 backdrop-blur-sm flex md:justify-center justify-between m-auto p-2 text-white'>
             <ul className='flex justify-center items-center'>
                 <li className='p-4 mr-10'>
-                    <a href="/#">
+                    <Link href="/#">
+                        <a>
                         <h2 className='text-3xl text-white font-semibold'>
                             <span className="invert">
                                 <Image  width="30" height="30" src="/icons8-cat-footprint-100.png" quality='100'>
                                 </Image>
                             </span>
                             &nbsp;Hao Jie</h2>
-                    </a>
+                        </a>
+                      
+                    </Link>
                 </li>
                 <li className='p-4 hidden md:block'>
                     <Link href='/'><a>&lt; About &#47;&gt;</a></Link>
