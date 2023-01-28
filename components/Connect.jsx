f993a9ce7b8979eaa274185544f6ac8f8099ab7d
Hao Jie <haojie9777@gmail.com>
2023-01-28 22:48:49 +0800

    styling

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -8,7 +8,7 @@ const Connect = () => {
         <h2 className='text-7xl font-bold mb-10'>Find Me
         </h2>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 20" fill="currentColor" className="w-7 h-7 animate-bounce">
-          <path fill-rule="evenodd" d="M14.77 4.21a.75.75 0 01.02 1.06l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0
+          <path fillRule="evenodd" d="M14.77 4.21a.75.75 0 01.02 1.06l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0
            011.08-1.04L10 8.168l3.71-3.938a.75.75 0 011.06-.02zm0 6a.75.75 0 01.02 1.06l-4.25 4.5a.75.75 0 01-1.08
             0l-4.25-4.5a.75.75 0 111.08-1.04L10 14.168l3.71-3.938a.75.75 0 011.06-.02z" clipRule="evenodd" />
         </svg>
