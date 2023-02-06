975dfd9be332ad888aaa0909f736fd7246dbeea4
Hao Jie <haojie9777@gmail.com>
2023-02-06 23:27:34 +0800

    styling refactoring

--- components/Connect.jsx
+++ components/Connect.jsx
@@ -12,29 +12,33 @@ const Connect = () => {
            011.08-1.04L10 8.168l3.71-3.938a.75.75 0 011.06-.02zm0 6a.75.75 0 01.02 1.06l-4.25 4.5a.75.75 0 01-1.08
             0l-4.25-4.5a.75.75 0 111.08-1.04L10 14.168l3.71-3.938a.75.75 0 011.06-.02z" clipRule="evenodd" />
         </svg>
-
-
       </div>
 
-      <div className='to-animate opacity-0 transition-all duration-1000'>
-        <a href="https://github.com/haojie9777" target="_blank" rel="noreferrer">
-          <button className='flex flex-row items-center p-1'>
-            <Image quality="100" width="50" height="50" src='/github-light.png'></Image>
-            <span className='m-5 text-cyan-400 text-lg'>@haojie9777</span>
-          </button>
-        </a>
-        <a href="https://www.linkedin.com/in/tanhaojie/" target="_blank" rel="noreferrer">
-          <button className='flex flex-row items-center rounded-md p-1'>
-            <Image quality="100" width="60" height="60" src='/linkedin-light.png'></Image>
-            <span className='m-5 text-cyan-400 text-lg'>Tan Hao Jie</span>
-          </button>
-        </a>
-        <a className="" href="mailto: haojie9777@gmail.com" target="_blank" rel="noreferrer">
-          <button className='flex flex-row items-center rounded-md p-1'>
-            <Image quality="100" width="50" height="50" src='/gmail-light.png'></Image>
-            <span className='m-5 text-cyan-400 text-lg'>haojie9777@gmail.com</span>
-          </button>
-        </a>
+      <div className='to-animate opacity-0 transition-all duration-1000 '>
+        <div className='ml-1'>
+          <a href="https://github.com/haojie9777" target="_blank" rel="noreferrer">
+            <button className='flex flex-row items-center p-1'>
+              <Image quality="100" width="50" height="50" src='/github-light.png'></Image>
+              <span className='m-5 text-cyan-400 text-lg'>@haojie9777</span>
+            </button>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/tanhaojie/" target="_blank" rel="noreferrer">
+            <button className='flex flex-row items-center p-1'>
+              <Image quality="100" width="60" height="60" src='/linkedin-light.png'></Image>
+              <span className='m-4 text-cyan-400 text-lg'>Tan Hao Jie</span>
+            </button>
+          </a>
+        </div>
+        <div className="ml-1">
+          <a href="mailto: haojie9777@gmail.com" target="_blank" rel="noreferrer">
+            <button className='flex flex-row items-center rounded-md p-1'>
+              <Image quality="100" width="50" height="50" src='/gmail-light.png'></Image>
+              <span className='m-6 text-cyan-400 text-lg'>haojie9777@gmail.com</span>
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,27 @@ module.exports = {
   theme: {
     backgroundImage: {
       'hero': "url('../public/images/nami.png')"
-    },
+    }, 
+    screens: {
+      'very-small-mobile': '375px',
+      
+      'mobile': '400px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   extend: {
     animation: {
